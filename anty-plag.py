738907fe46c8e36d69e3b1b48c4327d86b2f16ae
Zakhar <super.korustyvach@gmail.com>
2025-05-06 10:58:01 +0300

    second draft

--- anty-plag.py
+++ anty-plag.py
@@ -1,19 +1,30 @@
-def solve1(str1,  str2, thresh):
+def solve1(str1: str,  str2: str, thresh: int):
     m = len(str1)
     n = len(str2)
-    max_s = thresh
-    max_i, max_j = -1, -1
+    ans = []
     dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            if str1[i-1] != str2[j-1]:
+    for i in range(0, m+1):
+        for j in range(0, n+1):
+            if (i >= m or j >= n):
+                if dp[i][j] > thresh:
+                    ans.append((dp[i][j], i-dp[i][j], j-dp[i][j]))
                 continue
-            dp[i][j] = dp[i-1][j-1]+1
-            if dp[i][j] > max_s:
-                max_s = dp[i][j]
-                max_i, max_j = i-dp[i][j], j-dp[i][j]
+            if str1[i].capitalize() == str2[j].capitalize():  # next character validation
+                dp[i+1][j+1] = 1+dp[i][j]
+            elif dp[i][j] > thresh:
+                ans.append((dp[i][j], i-dp[i][j], j-dp[i][j]))
+    return ans
 
-    return max_s, max_i, max_j
+
+def sort_sols(sols):
+    boo = True
+    while boo:
+        boo = False
+        for i in range(len(sols)-1):
+            if sols[i][0] < sols[i+1][0]:
+                boo = True
+                sols[i], sols[i+1] = sols[i+1], sols[i]
+    return sols
 
 
 def main():
@@ -23,10 +34,11 @@ def main():
     with open('input_2.txt', 'r') as file:
         text_b = file.read()
         text_b.replace('\n', ' ')
-    max_s, max_i, max_j = solve1(text_a, text_b, -1)
-    print(max_s, max_i, max_j)
-    print(text_a[max_i:max_i+max_s])
-    print(text_b[max_j:max_j+max_s])
+    for sol in sort_sols(solve1(text_a, text_b, 10)):
+        max_s, max_i, max_j = sol
+        print(max_s, max_i, max_j)
+        print(text_a[max_i:max_i+max_s])
+        print(text_b[max_j:max_j+max_s])
 
 
 if __name__ == '__main__':
